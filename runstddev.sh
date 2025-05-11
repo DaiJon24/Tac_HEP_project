b221169c95037aefd2de3ddfa2ff4a2a8927d0cc
Walker Sundquist <wwsundquist@gmail.com>
2025-05-11 19:11:24 -0400

    Promote std_deviator_test to project

--- runstddev.sh
+++ runstddev.sh
@@ -1,4 +1,4 @@
 g++ -c std_deviator.cpp -o std_deviator.o
 g++ -c sliding_window.cpp -o sliding_window.o
-g++ std_deviator.o sliding_window.o std_deviator_test.cpp -o std_deviator
-./std_deviator
+g++ std_deviator.o sliding_window.o project.cpp -o project
+./project
