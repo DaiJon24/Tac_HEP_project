3ab51accb990722bd2843e8ae3b2f4ee23fe6aa0
Walker Sundquist <wwsundquist@gmail.com>
2025-05-11 19:00:31 -0400

    Update bash script to compile Julianne method

--- runstddev.sh
+++ runstddev.sh
@@ -1,3 +1,4 @@
 g++ -c std_deviator.cpp -o std_deviator.o
-g++ std_deviator.o std_deviator_test.cpp -o std_deviator
+g++ -c sliding_window.cpp -o sliding_window.o
+g++ std_deviator.o sliding_window.o std_deviator_test.cpp -o std_deviator
 ./std_deviator
