eb3b52b19217b8426ca6ec24a8360d3130301532
Julianne Starzee <jstarzee@cmstrigger02.hep.wisc.edu>
2025-05-11 16:47:33 -0500

    Updated Julianne method

--- sliding_window.cpp
+++ sliding_window.cpp
@@ -1,15 +1,26 @@
-#include "project.h" 
+#include "project.h"
 
 const int first_index = 10;
 const int second_index = 13;
 
-void find_maxima(int array[first_index][second_index], int output[first_index][second_index])
+int mod_row(int i, int rows){
+	return (i + rows) % rows;
+}
+
+void find_maxima(double array[first_index][second_index], std::vector<std::pair<int, int>>& coordinates)
 {       
-        for(int i = 1; i < first_index - 1; i++)
-        {       
-for (int j = 1; j < second_index - 1; j++){
-                        output[i][j] = 0;
-                        if (array[i - 1][j] < array[i][j] && array[i + 1][j] < array[i][j])
-{                                       
-if (array[i][j - 1] < array[i][j] && array[i][j + 1] < array[i][j])
-                {output[i][j] = 1;}}}}}
+	for (int i = 0; i < coordinates.size(); i++){
+		int x = coordinates[i][0];
+		int y = coordinates[i][1];
+		if (array[mod_row(x - 1, first_index)][y]  < array[x][y] && array[mod_row(x + 1, first_index)][y] < array[x][y]){
+			if (array[x][y - 1] < array[x][y] && array[x][y + 1] < array[x][y]){
+				double sum = 0;
+				for (int j = 0; j < 3; j++){
+					for (int k = 0; k < 3; k++){
+						sum += array[mod_row(x + j - 1, first_index)][y + k - 1];
+					}
+				}
+			}
+		}
+	}
+}
