7afe243f87928d0703cb3fb6bc2b0f2fe8280885
Walker Sundquist <wwsundquist@gmail.com>
2025-05-11 17:51:58 -0400

    Create project header

---
+++ project.h
@@ -0,0 +1,18 @@
+#ifndef PROJECT_H_
+#define PROJECT_H_
+
+#include <utility>
+#include <vector>
+
+void std_deviator(
+	double array[][],
+	double z,
+	std::vector<std::pair<int, int>>& coordinates
+	)
+
+void find_maxima(
+	double array[first_index][second_index],
+	std::vector<std::pair<int, int>>& coordinates
+	)
+
+#endif
