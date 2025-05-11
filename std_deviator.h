76558744829b609186f0e7a01cc9931dd6bf5750
Walker Sundquist <wwsundquist@gmail.com>
2025-05-11 17:38:41 -0400

    Add header for std_deviator

---
+++ std_deviator.h
@@ -0,0 +1,13 @@
+#ifndef STD_DEVIATOR_H_
+#define STD_DEVIATOR_H_
+
+#include <utility>
+#include <vector>
+
+void std_deviator(
+	double array[][],
+	double z,
+	std::vector<std::pair<int, int>>& coordinates
+	)
+
+#endif
